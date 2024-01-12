e18abf56afed072af36c4339403b5e1e8b71b82a
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2024-01-12 14:39:36 +0200

    12.01

--- src/main/java/com/example/lmstemplate01/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/lmstemplate01/configurations/SecurityConfiguration.java
@@ -4,10 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 /**
  * Security class. We permit and forbid access.
@@ -21,6 +20,10 @@ public class SecurityConfiguration extends AbstractHttpConfigurer {
                         .permitAll())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/example/lmstemplate01/controllers/AccountController.java
+++ src/main/java/com/example/lmstemplate01/controllers/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/account")
 public class AccountController {
     private final AccountService accountService;
 
@@ -18,26 +19,26 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    @PostMapping("/api/v1/account")
+    @PostMapping
     public ResponseEntity<ResultDTO> createAccount(@RequestBody AccountDTO accountDTO) {
         accountService.createAccount(accountDTO);
         return new ResponseEntity<>(new SuccessResultDTO(), HttpStatus.OK);
     }
 
-    @PatchMapping("/api/v1/account/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ResultDTO> updateAccount(@PathVariable(value = "id") Long accountId,
                                                    @RequestBody AccountDTO accountDTO) {
         accountService.updateAccount(accountDTO, accountId);
         return new ResponseEntity<>(new SuccessResultDTO(), HttpStatus.OK);
     }
 
-    @DeleteMapping("/api/v1/account/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<ResultDTO> deleteAccount(@PathVariable(value = "id") Long accountId) {
         accountService.deleteAccount(accountId);
         return new ResponseEntity<>(new SuccessResultDTO(), HttpStatus.OK);
     }
 
-    @GetMapping("/api/v1/account/{id}")
+    @GetMapping("/{id}")
     public AccountDTO retrieveAccount(@PathVariable(value = "id") Long accountId) {
         return accountService.getAccount(accountId);
     }
