7a452015c087fe4a3bdfe70360cbe05b8805d681
Dmytro Fedorov <mitya.fedorov1987@gmail.com>
2024-01-15 11:31:47 +0200

    1

--- src/main/java/com/example/lmstemplate01/controllers/RoleController.java
+++ src/main/java/com/example/lmstemplate01/controllers/RoleController.java
@@ -1,6 +1,5 @@
 package com.example.lmstemplate01.controllers;
 
-import com.example.lmstemplate01.dto.AccountDTO;
 import com.example.lmstemplate01.dto.ResultDTOPackege.BadResultDTO;
 import com.example.lmstemplate01.dto.ResultDTOPackege.ResultDTO;
 import com.example.lmstemplate01.dto.ResultDTOPackege.SuccessResultDTO;
@@ -10,10 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/role")
 @RequiredArgsConstructor
 public class RoleController {
